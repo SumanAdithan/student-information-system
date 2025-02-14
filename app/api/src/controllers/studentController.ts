3eca3225fbf48490b21a93e08c1c9f82be4995bd
Shijin2k3 <shijinkumar2003@gmail.com>
2025-02-14 10:46:35 +0530

    feat: Add global error handler

--- app/api/src/controllers/studentController.ts
+++ app/api/src/controllers/studentController.ts
@@ -1,19 +1,14 @@
+import { catchAsyncError } from '@middlewares';
 import { getAllStudents } from '@models';
 import { NextFunction, Request, Response } from 'express-serve-static-core';
 
-export const getStudents = async (req: Request, res: Response, next: NextFunction) => {
-    try{
-        const students=await getAllStudents();
-        res.status(200).json({
-            success:true,
-            students,
-        });
-    }catch(err){
-        console.error(err);
-        res.status(500).json({
-            success:false,
-            message:'Internal server Error',
-        })
-    }
+export const getStudents = catchAsyncError(async (req: Request, res: Response, next: NextFunction) => {
     
-};
+    const students=await getAllStudents();
+    res.status(200).json({
+        success:true,
+        students,
+    });
+    
+    
+});
--- app/api/src/createApp.ts
+++ app/api/src/createApp.ts
@@ -2,11 +2,13 @@ import express from 'express';
 import dotenv from 'dotenv';
 import path from 'path';
 import routes from '@routes';
+import { errorMiddleware } from '@middlewares';
 
 export const createApp = () => {
     const app = express();
     dotenv.config({ path: path.join(__dirname, 'config/config.env') });
 
     app.use('/api/v1',routes());
+    app.use(errorMiddleware);
     return app;
 };
