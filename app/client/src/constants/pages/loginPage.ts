749d08eceef95689bc94f9d7bb21f899b5c52b7a
SumanAdithan <sumanadithang34@gmail.com>
2025-02-25 09:18:41 +0530

    feat: initialized react frontend

---
+++ app/client/src/constants/pages/loginPage.ts
@@ -0,0 +1,11 @@
+import { logo } from '@assets';
+
+export const loginPageConfig = {
+    title: 'C.S.E DEPARTMENT',
+    subTitle: 'MET ENGINEERING COLLEGE',
+    logo: logo,
+    toggleTxt: {
+        student: 'Login as Faculty?',
+        faculty: 'Login as Student?',
+    },
+};
